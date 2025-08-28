5ad873e9c6f5a4034a120b133473d62a068dc31f
Romain Lefeuvre <romain.lefeuvre@inria.fr>
2025-08-28 15:19:34 +0200

    add workflow

---
+++ workflowsMSR/SAMPLE/10.1145_3524842.3527951.py
@@ -0,0 +1,21 @@
+from sampling_workflow.WorkflowBuilder import WorkflowBuilder
+from sampling_workflow.element.JsonLoader import JsonLoader
+from sampling_workflow.element.CsvWriter import CsvWriter
+from sampling_workflow.Metadata import Metadata
+from pathlib import Path
+
+from sampling_workflow.element.Loader import Loader
+def main():
+    url = Metadata.of_string("id")
+
+
+    workflow = (
+        WorkflowBuilder()
+        .input(Loader(
+            url,
+           
+        ))
+        .output(CsvWriter("rodrigues.csv"))
+    )
+
+    workflow.execute_workflow()
