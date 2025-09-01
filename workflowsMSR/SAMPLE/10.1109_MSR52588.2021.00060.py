49f258fbb6b05316f07c4849c16868fcd938b0af
Romain Lefeuvre <romain.lefeuvre@inria.fr>
2025-09-01 12:41:42 +0200

    Add workflows

---
+++ workflowsMSR/SAMPLE/10.1109_MSR52588.2021.00060.py
@@ -0,0 +1,20 @@
+from sampling_workflow.WorkflowBuilder import WorkflowBuilder
+from sampling_workflow.element.JsonLoader import JsonLoader
+from sampling_workflow.element.CsvWriter import CsvWriter
+from sampling_workflow.Metadata import Metadata
+from pathlib import Path
+
+
+
+
+
+def main():
+
+    # ---- Metadata ----
+    url = Metadata.of_string("id")
+    workflow = WorkflowBuilder().input(JsonLoader(
+            Path("Malavolt.json"),
+            url,
+        )).output(CsvWriter("out.csv"))
+    
+    workflow.execute_workflow()
